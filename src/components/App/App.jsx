ecfd56b319afbb3325eea05d20aaef0c69bea205
SergiyRybin <serhiobax@gmail.com>
2022-05-31 20:32:28 +0300

    upd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,32 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
     contacts: [],
-    name: '',
+    name: { name: '', id: '' },
   };
-  handleSubmit = (e)=>{
-    e.preventDefault()
-    const form = e.currentTarget;
-    const name = form.elements.name.value
-    this.setState({ name: name })
-    console.log(name);
-    console.log(this.state)
-  }
-  
-  handleChange= (e) =>{
-    // e.preventDefault()
-    // const { name, value } = e.target;
-    // this.setState({ [name]: value });
-    // console.log(e.target.value)
-  }
-  
 
+  handleSubmit = e => {
+    e.preventDefault();
+    const name = e.currentTarget.name.value;
+    this.setState(prev => {
+      return {
+        name: name,
+        id: this.loginInputId,
+        contacts: [...prev.contacts, name],
+      };
+    });
+    e.currentTarget.reset();
+  };
+  loginInputId = nanoid(5);
   render() {
+    const { contacts } = this.state;
     return (
       <div className="phoneBook">
         <h1>Phonebook</h1>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="">
+          <label>
             Name
             <input
               type="text"
@@ -35,14 +34,17 @@ class App extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
-              onChange={this.handleChange}
             />
           </label>
           <button type="submit">Add contact</button>
         </form>
 
         <h1>Contacts</h1>
-        <ul></ul>
+        <ul>
+          {contacts.map((contact,index) => (
+            <li key={index}>{contact}</li>
+          ))}
+        </ul>
       </div>
     );
   }
